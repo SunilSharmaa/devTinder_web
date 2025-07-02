0c488a0cdd547e58a108e4676d3f58f24145045c
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-07-02 20:20:01 +0530

    feat: install react-router-dom and set up basic routing with layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { Outlet, Route, Routes } from "react-router"
+import Body from "./components/Body"
+import Header from "./components/Header"
 
 function App() {
 
-  return (
-    <>
-      <h1>home</h1>
+  const Layout = () => {
+    return <>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
     </>
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Body />} />
+      </Route>  
+        <Route path="*" element={<h1>not found</h1>} />
+    </Routes>
   )
 }
 
