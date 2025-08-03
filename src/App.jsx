43eb6df2ec5ac819cf893e16de7ec1c0b2a2faf4
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-08-03 13:26:03 +0530

    add routes for Signin, Connection, and EditProfile; update layout with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import { Outlet, Route, Routes } from "react-router"
-import Body from "./components/Body"
-import Header from "./components/Header"
-import Signup from "./pages/Signup"
+import { Outlet, Route, Routes } from "react-router";
+import Body from "./components/Body";
+import Header from "./components/Header";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Footer from "./components/Footer";
+import Connection from "./pages/Connection";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
-
   const Layout = () => {
-    return <>
-    <Header />
-    <main>
-      <Outlet />
-    </main>
-    </>
-  }
+    return (
+      <>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </>
+    );
+  };
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Body />} />
-      </Route>  
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<h1>not found</h1>} />
+        <Route path="/connections" element={<Connection />} />
+        <Route path="/editProfile" element={<EditProfile />} />
+      </Route>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="*" element={<h1>not found</h1>} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
