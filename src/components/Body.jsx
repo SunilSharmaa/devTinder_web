83461d1a36e6e9933a1c39dbb76554db39edb188
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-08-03 13:34:51 +0530

    add api call to fetch user feed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Feed from './Feed'
+import axios from 'axios'
+import { DEVTINDER_BASE_URL } from '../utils/constants'
+import { useNavigate } from 'react-router'
+import { useDispatch } from 'react-redux'
+import { addFeedUser } from '../redux/feedSlice'
 
 const Body = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        const getFeed = async() => {
+            try{
+                const response = await axios.get(DEVTINDER_BASE_URL + "/user/feed", {
+                    withCredentials : true
+                })
+
+                dispatch(addFeedUser(response?.data?.data));
+            } catch (err) {
+                if(err.status === 401) {
+                    navigate("/signin")
+                }
+            }
+            
+        }
+
+        getFeed();
+    },[])
   return (
-    <div>Body</div>
+    <>
+        <Feed  />
+    </>
   )
 }
 
