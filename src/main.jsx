dddffb046443cdad0e9757666b56f44864bfd42f
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-08-03 12:26:23 +0530

    add redux store integration with provider in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import App from './App.jsx'
-import "./index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
