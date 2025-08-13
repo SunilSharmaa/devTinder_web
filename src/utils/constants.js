0452b2e0759f6272170e8c9110221060d6f9ff5d
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-08-13 20:11:51 +0530

    add env-based backend url selection for dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const DEVTINDER_BASE_URL = "http://localhost:7000";
+export const DEVTINDER_BASE_URL = 
+location.hostname === "localhost" ? 
+import.meta.env.VITE_DEVTINDER_BACKEND_DEVELOPMENT_URL : 
+import.meta.env.VITE_DEVTINDER_BACKEND_PRODUCTION_URL;
