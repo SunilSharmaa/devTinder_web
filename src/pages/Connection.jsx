5e3ca51fb506df9c4e1691a4009d6012b9dd180f
Ponnam Sharma <sunilsharma.workit@gmail.com>
2026-03-01 13:51:39 +0530

    refactor: seperate request and connection pages

--- src/pages/Connection.jsx
+++ src/pages/Connection.jsx
@@ -1,47 +1,53 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { DEVTINDER_BASE_URL } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addConnectionRequest } from "../redux/connectionSlice";
-import ConnectionRequest from "../components/ConnectionRequest";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addConnection } from '../redux/connectionSlice';
+import axios from 'axios';
+import { DEVTINDER_BASE_URL } from '../utils/constants';
+import ConnectionCard from '../components/ConnectionCard';
 
 const Connection = () => {
   const dispatch = useDispatch();
-  const connectionRequest = useSelector((state) => state.connectionReducer);
+  const connection = useSelector((state) => state.connection);
+  console.log(connection);
 
   useEffect(() => {
-    if (connectionRequest.length > 0) {
+    if (connection.length > 0) {
       return;
     }
-    const getConnectionRequest = async () => {
+    const getConnection = async () => {
       try {
         const response = await axios.get(
-          DEVTINDER_BASE_URL + "/user/request/received",
+          DEVTINDER_BASE_URL + "/user/connection",
           {
-            withCredentials: true,
+            withCredentials: true, 
           }
         );
-        // console.log(response?.data?.data);
-        dispatch(addConnectionRequest(response?.data?.data));
+        console.log(response?.data?.data);
+        const data = Array.isArray(response?.data?.data)
+        ? response.data.data
+        : [];
+
+        dispatch(addConnection(data));
       } catch (err) {
         console.log(err);
       }
     };
-    getConnectionRequest();
+    getConnection();
   }, []);
+
   return (
     <div className="min-h-screen text-xl pt-18">
       <div className="border py-10 w-[90%] mx-auto flex flex-col items-center rounded-lg space-y-2 ">
-        {connectionRequest.length === 0 ? (
-          <h1>no connection request found</h1>
+        {connection.length === 0 ? (
+          <h1>no connection found</h1>
         ) : (
-          connectionRequest?.map((user) => (
-            <ConnectionRequest user={user} key={user?._id} />
+          connection?.map((user) => (
+            <ConnectionCard user={user} key={user?._id}/>
           ))
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Connection;
+export default Connection
