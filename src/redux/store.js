48ddf486c2890264b9999863449d34939a5da433
Ponnam Sharma <sunilsharma.workit@gmail.com>
2026-03-07 13:57:12 +0530

    feat: update redux store with new slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import connectionRequestReducer from "./connectionRequestSlice";
 import connectionReducer from "./connectionSlice";
 
 const store = configureStore({
-  reducer: { userReducer, feedReducer, connectionReducer},
-
+  reducer: {
+    user: userReducer,
+    feed: feedReducer,
+    connectionRequest: connectionRequestReducer,
+    connection: connectionReducer,
+  },
 });
 
 export default store;
