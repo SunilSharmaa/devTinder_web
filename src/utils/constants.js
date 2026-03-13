ed9844681ded76c0093a317ab5214140372a838e
Ponnam Sharma <sunilsharma.workit@gmail.com>
2026-03-13 19:44:05 +0530

    refactor: use env mode instead of hostname for base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const DEVTINDER_BASE_URL = 
-location.hostname === "localhost" ? 
-import.meta.env.VITE_DEVTINDER_BACKEND_DEVELOPMENT_URL : 
-import.meta.env.VITE_DEVTINDER_BACKEND_PRODUCTION_URL;
+export const DEVTINDER_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEVTINDER_BACKEND_DEVELOPMENT_URL
+    : import.meta.env.VITE_DEVTINDER_BACKEND_PRODUCTION_URL;
