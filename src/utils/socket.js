497163e70edcdc08de5186ec35d5aee7ccef3915
Sunil Sharma <sunilsharma.workit@gmail.com>
2026-03-17 11:08:07 +0530

    refactor: use base url for socket connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,5 @@ import { io } from "socket.io-client";
 import { DEVTINDER_BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-    return io("http://localhost:7000/");
+    return io(DEVTINDER_BASE_URL);
 }
