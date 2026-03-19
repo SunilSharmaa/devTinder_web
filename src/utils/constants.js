23b6ad0072b595957e1cc598abc658064491ea82
Sunil Sharma <sunilsharma.workit@gmail.com>
2026-03-19 11:08:42 +0530

    refactor: hardcode local backend url for development

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const DEVTINDER_BASE_URL =
   import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_DEVTINDER_BACKEND_DEVELOPMENT_URL
-    : import.meta.env.VITE_DEVTINDER_BACKEND_PRODUCTION_URL;
+    ? "http://localhost:7000"
+    : "";
